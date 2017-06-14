abe54c495399a3375cb88cbfe56bcee2e5cbcc8d
Katherine Brown <katherine.e.brown.68@gmail.com>
2017-06-14 09:25:32 -0500

    KnowledgeBase.py updates

--- KnowledgeBase.py
+++ KnowledgeBase.py
@@ -4,7 +4,7 @@ import sys
 #method to import data into the MySQL server
 #Preconditions:
 # * login_file - a text file containing the login and database information
-#	Assumptions: On separate lines, the file must contain HOST, PORT, MySQL USER NAME, PASSWORD
+#	Assumptions: On separate lines, the file must contain HOST, PORT, MySQL USER NAME, PASSWORD, DATABASE
 # * data_file - a text file containing the data to be added to the databases
 #	Assumptions: The file is one comma-delimited record per line.
 #				 The first value for the line is the value to classify
@@ -12,8 +12,20 @@ import sys
 # * schema_file - a text file containing the MySQL schema for the table
 #	Assumptions: On separate lines, the file contains the MySQL schema for creation of the DATA table
 def importData(login_file, data_file, schema_file):
+	cursor = connectToDatabase(login_file)
+	print cursor
 	
+def connectToDatabase(login_file):
+	fileio = open(login_file, 'r')
+	host = fileio.readline()
+	port = fileio.readline()
+	user = fileio.readline()
+	passw = fileio.readline()
+	database = fileio.readline()
 
+	db = MySQLdb.connect(host, port, user, passw, database)
+	cursor =  db.cursor()
+	return cursor
 	
 def main():
 	importData("config/login_template.txt", "data/SPECT.data", "data/SPECT.schema")
