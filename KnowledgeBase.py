a41e958ab07b8d9a6c5a5d49c137b00fd7101056
Katherine Brown <katherine.e.brown.68@gmail.com>
2017-06-14 09:08:26 -0500

    Updating data directory

--- KnowledgeBase.py
+++ KnowledgeBase.py
@@ -1,12 +1,22 @@
 import MySQLdb
+import MySQLdb.cursors
 import sys
-
-def importData(login_file, data_file):
-	print("stub function")
+#method to import data into the MySQL server
+#Preconditions:
+# * login_file - a text file containing the login and database information
+#	Assumptions: On separate lines, the file must contain HOST, PORT, MySQL USER NAME, PASSWORD
+# * data_file - a text file containing the data to be added to the databases
+#	Assumptions: The file is one comma-delimited record per line.
+#				 The first value for the line is the value to classify
+#				 The remaining lines are the attributes
+# * schema_file - a text file containing the MySQL schema for the table
+#	Assumptions: On separate lines, the file contains the MySQL schema for creation of the DATA table
+def importData(login_file, data_file, schema_file):
+	
 
 	
 def main():
-	importData("test.txt", "test.txt")
+	importData("config/login_template.txt", "data/SPECT.data", "data/SPECT.schema")
 
 if __name__ == "__main__":
 	main()
