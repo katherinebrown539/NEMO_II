1c03329172997e9906fe530aecc7b12fa51a2228
Katherine Brown <katherine.e.brown.68@gmail.com>
2017-06-14 08:32:40 -0500

    Added stub of Knowledge Base file

---
+++ KnowledgeBase.py
@@ -0,0 +1,12 @@
+import MySQLdb
+import sys
+
+def importData(login_file, data_file):
+	print("stub function")
+
+	
+def main():
+	importData("test.txt", "test.txt")
+
+if __name__ == "__main__":
+	main()
