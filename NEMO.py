581e5f75ffbb9864f5f8071ea4a943c2e4bf20cd
Katherine Brown <katherine.e.brown.68@gmail.com>
2017-06-15 11:25:17 -0500

    more edits...

--- NEMO.py
+++ NEMO.py
@@ -6,6 +6,7 @@ def main():
 	kb = KnowledgeBase("config/login_file.txt")
 	kb.importData("data/SPECT.data", "data/SPECT.schema")
 	#importData("config/login_file.txt", "data/SPECTF.data", "data/SPECTF.schema")
-
+	ml = ML_Controller(kb)
+	
 if __name__ == "__main__":
 	main()
