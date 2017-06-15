6084b632904a5cf1ada74f0e51ad6613fd59e5fe
Katherine Brown <katherine.e.brown.68@gmail.com>
2017-06-15 17:32:39 -0500

    Forgot an import

--- SciKit_Controller.py
+++ SciKit_Controller.py
@@ -1,3 +1,4 @@
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report,confusion_matrix
