5312d3fb14136a83b920e6bbd2a0f20e495931ca
Katherine Brown <katherine.e.brown.68@gmail.com>
2017-07-03 09:09:24 -0500

    Beginning work on enhancing NEMO for background optimization

--- KBCreator.sql
+++ KBCreator.sql
@@ -2,10 +2,19 @@ use NEMO_KB;
 
 drop table if exists AlgorithmResults;
 create table AlgorithmResults(
-	algorithm_id varchar(1000),
+	algorithm_id int,
 	algorithm_name varchar(255), 
 	accuracy double, 
 	prec double, 
 	recall double, 
 	f1 double, 
 	confusion_matrix varchar(255));
+
+/*
+drop table if exists ModelRepository;
+create table ModelRepository(
+	algorithm_id int primary key,
+	algorithm_name varchar(255),
+	args varchar(1000) --this stores all the arguments required to recreate the model. . .
+);
+*/
--- NEMO.py
+++ NEMO.py
@@ -1,18 +1,59 @@
 from KnowledgeBase import KnowledgeBase
 from ML_Controller import ML_Controller
 import sys
+	
+class NEMO:
+	def __init__(self, filename):
+		self.kb = KnowledgeBase(filename)
+		self.ml = None #will be a list
+		self.thread = None #will be a list
+		self.event = None #will be a list
+		
+	def setupNewML(self):
+		self.ml = ML_Controller(self.kb)
+		#return ml
+		
+	def runAlgorithm(self):
+		self.ml.runAlgorithm()
+		
+	def optimizeAlgorithm(self):
+		self.ml.optimizeAlgorithm()
+
+	def menu():
+		menu = "Main Menu:\n1. Create New Model\n2. Run New Model\n3. Optimize the model\n4. Output Model Results (Any current optimization task will be halted)\n5. Cancel All Optimization Tasks\n6. Quit NEMO"
+		choices = ["1","2","3","4","5","6"]
+		choice = ""
+		while choice not in choices:
+			choice = raw_input(menu)
+		if choice == "1":
+			self.setUpNewML()
+		elif choice == "2":
+			self.runAlgorithm()
+		elif choice == "3":
+			print "Need to set up multi-threading for background optimization"
+			self.optimizeAlgorithm()
+		elif choice == "4":
+			print "Print out algorithm results here"
+		elif choice == "5":
+			print "When multi-threading background optimization is setup, this will stop any optimization tasks"
+		else:
+			sys.exit()
 		
 def main():
-	kb = KnowledgeBase("config/config.json")
-	#kb.importData("data/SPECT.data", "data/SPECT.schema")
-	#importData("config/login_file.txt", "data/SPECTF.data", "data/SPECTF.schema")
-	ml = ML_Controller(kb)
-	ml.runAlgorithm()
+	nemo = NEMO("config/config.json")
+	while True:
+		nemo.menu()
+
+def old_main():
+	nemo = NEMO("config/config.json")
+	ml = nemo.setupNewML()
+	
+	nemo.runAlgorithm()
 	
 	try:
 		while True:
 			try:
-				ml.optimizeAlgorithm()
+				nemo.optimizeAlgorithm()
 			except:
 				sys.exit()
 	except:
