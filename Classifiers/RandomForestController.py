cf55d576086ff38cfe8345f2d2189caf73ce91af
Katherine Brown <katherine.e.brown.68@gmail.com>
2017-07-31 15:35:05 -0400

    More work on RandomForest Implementation

--- Classifiers/RandomForestController.py
+++ Classifiers/RandomForestController.py
@@ -28,13 +28,89 @@ class RandomForestController:
 		pass
 	
 	def createModelFromID(self, x, y, id):
+
+		
+		# criterion - string in  ['gini', 'entropy']
+
+		# warm_start - bool
+		# class_weight - dict, list of dicts, "balanced"
+		
+		#run query
+		stmt = "select * from ModelRepository where algorithm_id = " + id
+		self.kb.executeQuery(stmt)
+		row = self.kb.fetchOne()
+		attributes = {}
+		while row != None:
+			#print row
+			key = row[2]
+			val = row[3]
+			#print key + ": " + val
+			if val == 'None' or val == 'NULL':
+				val = None
+			if val is not None:
+				if key == 'max_features':
+					lst = list(val)
+					if lst.count('.') > 0:
+						val = float(val)
+					elif val in ['auto', 'sqrt', 'log2']:
+						val = val
+					else:
+						val = int(val)
+				if key in ['max_depth', 'max_leaf_nodes', 'random_state', 'n_estimators', 'max_leaf_nodes', 'n_jobs', 'random_state', 'verbose']:
+					val = int(val)
+				elif key in ['min_weight_fraction_leaf', 'min_impurity_split']:
+					val = float(val)
+				elif key in ['min_samples_split', 'min_samples_leaf']:
+					lst = list(val)
+					if lst.count('.') > 0:
+						val = float(val)
+					else:
+						val = int(val)	
+				elif key in ['boostrap', 'oob_score', 'warm_start']:
+					val = (val == "True")
+				elif key in ['class_weight']:
+					lst = list(val)
+					if lst.count('{') == 1:
+						val = dict(val)
+					elif lst.count('{') > 1:
+						val = list(val)
+						for i in range(0,len(val)):
+							val[i] = dict(val[i])						
+			#print type(val)
+ 			attributes[key] = val
+			row = self.kb.fetchOne()
+		self.createModel(x,y,attributes)
 		pass
 	
 	def copyModel(self,x,y,id):
-		pass
+		self.algorithm_id = id
+		self.createModelFromID(x,y,id)
 	
 	def runModel(self, multi=False, x = None, y = None,):
-		pass
+		if x is not None:
+			self.X_test = x
+			self.y_test = y
+		av = ''
+		if not multi:
+			av = 'binary'
+		else:
+			av = 'micro'
+		
+		print "Number of test instances: " + str(len(self.X_test.index))
+		
+		
+		predictions = self.forest.predict(self.X_test)	
+		accuracy = accuracy_score(self.y_test,predictions)
+		precision = precision_score(self.y_test,predictions, average=av)
+		recall = recall_score(self.y_test, predictions, average=av)
+		f1 = f1_score(self.y_test,predictions, average=av)
+		cm = confusion_matrix(self.y_test,predictions)
+		
+		self.results = {'ID': self.algorithm_id, 'Name': self.algorithm_name, 'Accuracy': accuracy, 'Precision': precision, 'Recall': recall, 'F1': f1, 'Confusion_Matrix': cm}
+		
+		to_return =  (self.algorithm_id, self.algorithm_name, accuracy, precision, recall, f1, cm)
+		self.kb.removeCurrentModel(self)
+		return to_return
 		
 	def coordinateAscent(self, metric):
 		pass
