02f4ef8db37fa0ea9a21de7fe1978c85980b4d29
Katherine Brown <katherine.e.brown.68@gmail.com>
2017-09-15 13:31:29 -0500

    Edits to NELController.py

--- NELController.py
+++ NELController.py
@@ -12,7 +12,7 @@ class NELController:
         with open(facts_file) as fd:
             json_data = json.load(fd)
 
-        print (json_data)
+        #print (json_data)
         for classifier in json_data:
             print (classifier)
 
