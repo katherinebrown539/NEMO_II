248df3c92ae1024f4035a82b18308ddabb3280d1
Katherine Brown <katherine.e.brown.68@gmail.com>
2017-09-15 10:50:29 -0500

    First test of NELController.py

--- NELController.py
+++ NELController.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python
 from KnowledgeBase import KnowledgeBase
 from Classifiers import ML_Controller, KnowledgeIntegrator
+import MySQLdb
 from collections import deque
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
-import MySQLdb, threading, sys, os, time, json, traceback, pandas, numpy
+import threading, sys, os, time, json, traceback, pandas, numpy
 
 class NELController:
-    
+    def __init__(facts_file):
+        with open(facts_file) as fd:
+            json_data = json.load(fd)
+
+        for classifier in json_data:
+            print (classifier)
+
+def main():
+    filename = "config/facts.json"
+    NELController(filename)
+
+if __name__ == '__main__':
+    main()
