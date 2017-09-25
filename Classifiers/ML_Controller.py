0b311dcdf05f02faeb5e55745d4907c19d0784ee
Katherine Brown <katherine.e.brown.68@gmail.com>
2017-09-25 13:51:22 -0500

    updates

--- Classifiers/ML_Controller.py
+++ Classifiers/ML_Controller.py
@@ -23,8 +23,8 @@ class ML_Controller:
 	#imports data from knowledge base
 	#Preconditions:
 	# * A knowledge base has been set up and read data
-	#Postconditions: 
-	# * Data will be imported from the 
+	#Postconditions:
+	# * Data will be imported from the
 	def __init__(self, kb, algorithm_type):
 		cols = ",".join(kb.X)
 		stmt = "select " + cols + " from " + kb.name + ";"
@@ -40,7 +40,7 @@ class ML_Controller:
 		self.kb = kb
 		self.isCurrentlyOptimizing = False	#True when model is in optimization queue, false otherwise
 		self.algorithm = None
-		#print algorithm_type
+		print algorithm_type
 		if algorithm_type == "Neural Network":
 			self.algorithm = NeuralNetworkController.NeuralNetworkController(self.kb)
 		if algorithm_type == "Decision Tree":
@@ -63,45 +63,45 @@ class ML_Controller:
 		stmt = "select " + self.kb.Y + " from " + self.kb.name
 		#print stmt
 		self.target = pandas.read_sql_query(stmt, self.kb.db)
-			
+
 	def get_params(self):
 		return self.algorithm.get_params()
-	
+
 	def createModel(self, id=None):
 		if id is None:
 			self.algorithm.createModel(self.data, self.target)
 		else:
 			self.algorithm.createModelFromID(self.data, self.target, id)
-	
+
 	def createModelPreSplit(self, xtrain, xtest, ytrain, ytest, attributes=None):
 		if attributes is None and self.algorithm.isModelCreated():
 			attributes = self.get_params()
 		self.algorithm.createModelPreSplit(xtrain, xtest, ytrain, ytest, attributes)
-	
+
 	def copyModel(self, id):
 		self.algorithm.copyModel(self.data, self.target, id)
-	
+
 	def fit(self, x, y):
-		
+
 		self.algorithm.fit(x,y)
-	
+
 	def predict(self, x):
 		return self.algorithm.predict(x)
-	
+
 	def runAlgorithm(self, x = None, y = None):
 		results = self.algorithm.runModel(self.kb.multi, x, y)
 		#self.kb.updateDatabaseWithResults(self.algorithm)
 		return results
-		
+
 	def updateDatabaseWithResults(self):
 		self.kb.updateDatabaseWithResults(self.algorithm)
-	
+
 	def getName(self):
 		return self.algorithm.algorithm_name
-		
+
 	def getID(self):
 		return self.algorithm.algorithm_id
-		
+
 	def optimizeAlgorithm(self):
 		curr_id = self.algorithm.algorithm_id
 		self.algorithm = self.algorithm.optimize('Accuracy', 'Coordinate Ascent')
