b10b5db202485ec66b32957fffe4d0590bc2a016
Katherine Brown <katherine.e.brown.68@gmail.com>
2017-09-25 13:47:14 -0500

    updates

--- NELController.py
+++ NELController.py
@@ -20,7 +20,7 @@ class NELController:
         for classifier in classifiers:
             created_classifier = self.createClassifier(classifier)
             self.classifiers.append(created_classifier)
-            print created_classifier.runModel()
+            print created_classifier['Classifier'].runModel()
             #run classifiers
 
         #parse constraints
