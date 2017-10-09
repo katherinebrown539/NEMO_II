f4700b35d1dfb49d7152aa5ad4f9025215047dd9
Katherine Brown <katherine.e.brown.68@gmail.com>
2017-10-09 14:09:26 -0500

    updates

--- NELController.py
+++ NELController.py
@@ -48,6 +48,17 @@ class NELController:
         KI_Lung = KnowledgeIntegrator.KnowledgeIntegrator(lung_kb, lung_blanket['CLASSIFIERS_THAT_INFLUENCE'], stacking_classifier='Decision Tree', other_predictions=None, use_features=False)
         KI_Breast = KnowledgeIntegrator.KnowledgeIntegrator(breast_kb, breast_blanket['CLASSIFIERS_THAT_INFLUENCE'], stacking_classifier='Decision Tree', other_predictions=None, use_features=False)
         #run KIs
+        data = kb.getData()
+        shuffled_data = shuffle(data)
+        splits = numpy.array_split(shuffled_data, 10)
+        num_folds = 10
+        random_seed = 0
+        results = []
+        results.append(KI_Lung.testKI(splits, num_folds, random_seed))
+        results.append(KI_Breast.testKI(splits, num_folds, random_seed))
+        for r in results:
+            print r
+
     def generateMarkovBlanket(self):
         #create blanket dicts
         self.blankets = []
