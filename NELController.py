5b8fc4eb0159cf8be283a18a2ef2a3b5564e7e7b
Katherine Brown <katherine.e.brown.68@gmail.com>
2017-10-12 14:44:39 -0500

    updates

--- NELController.py
+++ NELController.py
@@ -138,7 +138,7 @@ class NELController:
         new_kb.X = x
         new_kb.Y = y
         print("Algorithm: " + algorithm)
-        ml = ML_Controller.ML_Controller(kb, algorithm)
+        ml = ML_Controller.ML_Controller(new_kb, algorithm)
         ml.createModel()
         d =  {"Classifier_Name": classifier_name, "Class": target, "Classifier": ml}
         #print d
