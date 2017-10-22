a70595d08ede9b72f5f25f1abd9ce9661f8348c5
Katherine Brown <katherine.e.brown.68@gmail.com>
2017-10-22 15:01:55 -0500

    Updates

--- Classifiers/ML_Controller.py
+++ Classifiers/ML_Controller.py
@@ -38,6 +38,7 @@ class ML_Controller:
 		self.kb = kb
 		self.isCurrentlyOptimizing = False	#True when model is in optimization queue, false otherwise
 		self.algorithm = None
+		self.name = self.kb.name + "_" + self.algorithm_type
 		print (algorithm_type)
 		if algorithm_type == "Neural Network":
 			self.algorithm = NeuralNetworkController.NeuralNetworkController(self.kb)
