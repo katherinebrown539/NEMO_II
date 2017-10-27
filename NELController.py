6835ad5781ef4bfd59e274c7afcb82913fab74e2
Katherine Brown <katherine.e.brown.68@gmail.com>
2017-10-27 18:21:26 -0500

    Updates

--- NELController.py
+++ NELController.py
@@ -31,7 +31,7 @@ class NELController:
         self.blankets = []
         self.parseConstraints(json_data['Constraints'])
         self.generateMarkovBlanket()
-        self.runBlanketsInKI()
+        #self.runBlanketsInKI()
         #self.execute()
         sys.stdout = save_stdout
         # for r in self.results:
@@ -399,6 +399,7 @@ class NELController:
         return d
 
     def runModel(self, classifier):
+        print("Training: " + classifier.name)
         accs = []
         precs = []
         recs = []
