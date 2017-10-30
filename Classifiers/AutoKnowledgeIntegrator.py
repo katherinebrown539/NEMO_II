e7fe23049baec00939dcb638894dea2b2d9965fd
Katherine Brown <katherine.e.brown.68@gmail.com>
2017-10-30 11:38:28 -0500

    updates

--- Classifiers/AutoKnowledgeIntegrator.py
+++ Classifiers/AutoKnowledgeIntegrator.py
@@ -56,7 +56,7 @@ class AutoKnowledgeIntegrator:
             holdout.index = list(range(len(holdout)))
             print("Training length: " + str(len(train)))
             print("Holdout length: " + str(len(holdout)))
-            train.to_csv("test_data/"+self.name+"_train_"+str(id_)+".csv")
+            #train.to_csv("test_data/"+self.name+"_train_"+str(id_)+".csv")
             temp_results = self.cv_step(train, holdout, train_index, k, random_seed, id_)
             results['Accuracy'].append(temp_results['Accuracy'])
             results['Precision'].append(temp_results['Precision'])
@@ -104,7 +104,7 @@ class AutoKnowledgeIntegrator:
                 print("Original y length " + str(len(y_cls)))
                 y_cls = y_cls.iloc[train_index_] #reducing all y to training y
                 print("New y length " + str(len(y_cls)))
-                pandas.concat(objs=[x_cls,y_cls], axis=1).to_csv("test_data/sub_classifier"+classifier.kb.Y+"_train_"+str(id_))+".csv"
+                #pandas.concat(objs=[x_cls,y_cls], axis=1).to_csv("test_data/sub_classifier"+classifier.kb.Y+"_train_"+str(id_))+".csv"
                 y_cls_train,y_cls_test = y_cls.iloc[train_index], y_cls.iloc[test_index]
                 classifier.fit(train_x_train, y_cls_train)
                 predictions[i].extend(classifier.predict(train_x_test))
