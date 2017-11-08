0356f5eb669cb5b45a8e74a4b411307602284b71
Katherine Brown <katherine.e.brown.68@gmail.com>
2017-11-07 19:18:50 -0600

    Update

--- test_significance.py
+++ test_significance.py
@@ -1,5 +1,5 @@
 from NELController import NELController
-for i in range(1,8):
-    print("Iteration #"+str(i))
-    output_file = "significance_test/stacked/results_"+str(i)
+for i in range(0,8):
+    print("Iteration #"+str(i+1))
+    output_file = "significance_test/stacked/results_"+str(i+1)
     NELController("config/facts.json", "config/config.json", output_file)
