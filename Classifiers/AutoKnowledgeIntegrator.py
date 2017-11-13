5a2cb0e1e3431f26a5f497fa6e19d6f55ca9953a
Katherine Brown <katherine.e.brown.68@gmail.com>
2017-11-13 13:45:55 -0600

    updates

--- Classifiers/AutoKnowledgeIntegrator.py
+++ Classifiers/AutoKnowledgeIntegrator.py
@@ -177,7 +177,7 @@ class AutoKnowledgeIntegrator:
     def getFeatures(self, X):
         rand_int = random.randint(0,100)
         r = SelectFromModel(self.stacking_classifier, prefit=True)
-        r_ = r.transform(X)
+        r_ = pandas.DataFrame(r.transform(X))
         r_.to_csv(path_or_buf="features/"+self.name+str(rand_int)+".csv", delimiter=",")
 
 
