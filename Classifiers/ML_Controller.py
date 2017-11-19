0b37c59c64ae7f8a0b0bc6028ab901b11355f218
Katherine Brown <katherine.e.brown.68@gmail.com>
2017-11-19 11:21:57 -0600

    updates

--- Classifiers/ML_Controller.py
+++ Classifiers/ML_Controller.py
@@ -50,6 +50,7 @@ class ML_Controller:
 			self.algorithm = RandomForestController.RandomForestController(self.kb)
 		elif algorithm_type == "Logistic Regression":
 			self.algorithm = LogisticRegressionController.LogisticRegressionController(self.kb)
+			print(type(self.algorithm))
 		else:
 			self.algorithm = DecisionTreeController.DecisionTreeController(self.kb)
 
